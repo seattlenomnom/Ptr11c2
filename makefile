02a20d007dd34ff631f2ae2f5ab73cd22126a231
seattlenomnom <mark.crapser@gmail.com>
2023-05-23 22:41:13 -0700

    first commit

---
+++ makefile
@@ -0,0 +1,3 @@
+# makefile for ptr11c2
+ptr11c2: ptr11c2
+	gcc -g ptr11c2.c -o ./Build/ptr11c2
---
+++ ptr11c2.c
@@ -0,0 +1,17 @@
+/* ptr11c2.c
+ * C file for ptr11c2, a program to explore interesting properites of
+ * pointers. P.238 Kochan, Stephen, "Programming in C Third Ed."
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    char c = 'Q';
+    char *char_pointer = &c;
+
+    printf("%c %c\n", c, *char_pointer);
+
+
+
+
+}
